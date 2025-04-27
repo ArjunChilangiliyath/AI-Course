f2af293889dfbc3f72bd07d42771ab27c0bb2e33
arjunchilangiliyath <22bb15176@rit.ac.in>
2025-04-27 13:56:29 +0530

    Create positive_numbers.py

---
+++ positive_numbers.py
@@ -0,0 +1,14 @@
+list1 = [12, -7, 5, 64, -14]
+positive_numbers1 = []
+print("Input: list1 =", list1)
+for num in list1:
+    if num > 0:
+        positive_numbers1.append(num)
+print("Output:", positive_numbers1)
+list2 = [12, 14, -95, 3]
+positive_numbers2 = []
+print("\nInput: list2 =", list2)
+for num in list2:
+    if num > 0:
+        positive_numbers2.append(num)
+print("Output:", positive_numbers2)
